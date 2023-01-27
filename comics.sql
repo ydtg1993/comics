4e327abcc3e42e6fe2f3cacd37a45d44bfc957c2
hikki <hikki@qqc88.net>
2023-01-27 17:46:11 +0800

    v1

--- comics.sql
+++ comics.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : mydb
+ Source Server         : loc
  Source Server Type    : MySQL
- Source Server Version : 50735
- Source Host           : localhost:3306
+ Source Server Version : 50737
+ Source Host           : 127.0.0.1:3306
  Source Schema         : comics
 
  Target Server Type    : MySQL
- Target Server Version : 50735
+ Target Server Version : 50737
  File Encoding         : 65001
 
- Date: 27/01/2023 07:42:38
+ Date: 27/01/2023 17:45:45
 */
 
 SET NAMES utf8mb4;
@@ -26,39 +26,62 @@ CREATE TABLE `source_chapter`  (
   `source` tinyint(1) NOT NULL DEFAULT 1 COMMENT '采集源 1:快看 2:腾讯',
   `source_id` int(11) NOT NULL COMMENT '源漫画id',
   `source_chapter_id` int(11) NOT NULL COMMENT '源章节id',
-  `source_uri` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '源uri',
+  `source_uri` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '源uri',
   `cover` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '封面',
-  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '标题',
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '标题',
+  `updated_at` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `source_chapter_id`(`source`, `source_id`, `source_chapter_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '采集-漫画章节' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of source_chapter
+-- Table structure for source_chapter_ext
 -- ----------------------------
+DROP TABLE IF EXISTS `source_chapter_ext`;
+CREATE TABLE `source_chapter_ext`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `chapter_id` int(11) NOT NULL,
+  `sort` int(11) NOT NULL DEFAULT 0,
+  `is_free` tinyint(4) NOT NULL DEFAULT 0 COMMENT '0免费 1收费',
+  `source_data` json NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for source_comic
 -- ----------------------------
 DROP TABLE IF EXISTS `source_comic`;
 CREATE TABLE `source_comic`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `source` tinyint(1) NOT NULL DEFAULT 1 COMMENT '采集源 1:快看 2:腾讯',
   `source_id` int(11) NOT NULL COMMENT '源漫画id',
-  `source_uri` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '源uri',
+  `source_uri` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '源uri',
   `cover` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '封面',
-  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '标题',
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '标题',
+  `updated_at` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `source_id`(`source`, `source_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '采集-漫画' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of source_comic
+-- Table structure for source_comic_ext
 -- ----------------------------
+DROP TABLE IF EXISTS `source_comic_ext`;
+CREATE TABLE `source_comic_ext`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `comic_id` bigint(20) NOT NULL,
+  `author` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '作者',
+  `category` json NOT NULL COMMENT '分类',
+  `chapter_count` int(11) NOT NULL DEFAULT 0 COMMENT '章节数量',
+  `like_count` int(11) NOT NULL DEFAULT 0 COMMENT '喜欢',
+  `popularity` int(11) NOT NULL DEFAULT 0 COMMENT '人气热度',
+  `is_free` tinyint(1) NULL DEFAULT 0 COMMENT '0免费 1收费',
+  `description` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '描述',
+  `source_data` json NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for source_image
@@ -69,14 +92,11 @@ CREATE TABLE `source_image`  (
   `source` tinyint(1) NULL DEFAULT NULL COMMENT '采集源 1:快看 2:腾讯',
   `source_id` int(11) NOT NULL,
   `source_chapter_id` int(11) NOT NULL,
+  `domain` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
   `images` json NOT NULL,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of source_image
--- ----------------------------
 
 SET FOREIGN_KEY_CHECKS = 1;
