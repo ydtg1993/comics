4eaf2cee1374ea76aa1e5378e60f655def024fb7
hikki <hikki@qqc88.net>
2023-02-01 13:47:35 +0800

    v1

--- robot/action.go
+++ robot/action.go
@@ -1,12 +1,6 @@
 package robot
 
 import (
-	"comics/tools/config"
-	"compress/gzip"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"strings"
 	"time"
 )
 
@@ -25,39 +19,4 @@ func (Robot *Robot) TapIn(method, path string) bool {
 		}
 	}
 	return false
-}
-
-func (Robot *Robot) CatchImage(url string) []byte {
-	payload := strings.NewReader("")
-	req, _ := http.NewRequest("GET", url, payload)
-	req.Header.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
-	req.Header.Add("Accept-Encoding", "gzip, deflate")
-	req.Header.Add("Accept-Language", "zh-CN,zh;q=0.9")
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded;charset=utf-8")
-	currentUrl, _ := Robot.WebDriver.CurrentURL()
-	req.Header.Add("Referer", currentUrl)
-	req.Header.Add("Origin", currentUrl)
-	req.Header.Add("Host", currentUrl)
-	req.Header.Add("User-Agent", config.Spe.UserAgent)
-	req.Header.Add("X-Requested-With", "XMLHttpRequest")
-	req.Header.Add("Connection", "keep-alive")
-	req.Header.Add("Cache-Control", "no-cache")
-
-	res, err := http.DefaultClient.Do(req)
-	defer res.Body.Close()
-
-	var response []byte
-	if err != nil {
-		fmt.Println(err.Error())
-		return response
-	}
-
-	reader, err := gzip.NewReader(res.Body)
-	if err != nil {
-		fmt.Println(err.Error())
-		return response
-	}
-	response, _ = ioutil.ReadAll(reader)
-
-	return response
 }
